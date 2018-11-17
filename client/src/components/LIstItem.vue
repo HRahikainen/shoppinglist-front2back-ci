78f7b8d313a1e1a37192be7ee2353176ee6b74b6
HRahikainen <rahikainen.harri@gmail.com>
2018-11-17 23:37:55 +0200

    Initial frontend setup with Vue-CLI.

---
+++ client/src/components/LIstItem.vue
@@ -0,0 +1,17 @@
+<template>
+    <li class="collection-item">
+        <div class="valign-wrapper">
+            <p style="word-break:break-all; width:100%;">{{ name }}</p>
+            <i v-on:click="$emit(\'remove\')" class="secondary-content tiny material-icons" style="cursor:pointer;">clear</i>
+        </div>
+    </li>
+</template>
+
+<script>
+export default {
+  name: 'ListItem',
+  props: {
+    name: String
+  }
+}
+</script>
