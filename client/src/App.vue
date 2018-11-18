ecadedff3b080300078b95d134e8e2de8dffeeca
HRahikainen <rahikainen.harri@gmail.com>
2018-11-18 17:57:10 +0200

    Deleted default components and added own custom to App.

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,28 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ListItem name="Item 1"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ListItem from './components/ListItem.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    ListItem
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  
 }
 </style>
