f424a7541b4aeed75f4ec25c9582ee9ec602ba6c
HRahikainen <rahikainen.harri@gmail.com>
2018-12-05 12:04:55 +0200

    Created some auth files following the tutorial.

--- server/app.js
+++ server/app.js
@@ -1,10 +1,12 @@
 "use strict";
 
 const express = require("express");
+const session = require("express-session");
 const bodyParser = require("body-parser");
 const path = require("path");
 const app = express();
 const cors = require("cors");
+const errorHandler = require("errorhandler");
 
 const indexRouter = require("./routes/index");
 //Set up mongoose connection
@@ -18,10 +20,14 @@ mongoose.Promise = global.Promise;
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
+require("./models/Users");
+require("./config/passport");
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
+//app.use(session({ secret: 'passport-tutorial', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false }));
+//app.use(express.static(path.join(__dirname, "public")));
 // app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
 app.use("/", indexRouter);
 const PORT = process.env.PORT || 3000;
