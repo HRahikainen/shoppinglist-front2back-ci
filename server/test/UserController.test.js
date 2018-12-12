b0b9660ab36d2f4f70d961ef6ae1fe19510a8198
HRahikainen <rahikainen.harri@gmail.com>
2018-12-12 11:09:00 +0200

    Travis.yml change test.

--- server/test/UserController.test.js
+++ server/test/UserController.test.js
@@ -3,7 +3,7 @@ const request = require("supertest").agent(app);
 const mongoose = require("mongoose");
 
 describe("Test the logout path", () => {
-  afterAll(async () => await mongoose.disconnect());
+  //afterAll(async () => await mongoose.disconnect());
 
   test("It should response the GET method", done => {
     return request.get("/users/logout").then(response => {
