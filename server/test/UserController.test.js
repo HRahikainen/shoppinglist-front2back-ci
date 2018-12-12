39bb24a9f0aa44df579ac16444bfa586f1e8bb07
HRahikainen <rahikainen.harri@gmail.com>
2018-12-12 10:59:42 +0200

    Added two user route tests.

--- server/test/UserController.test.js
+++ server/test/UserController.test.js
@@ -11,4 +11,24 @@ describe("Test the logout path", () => {
       done();
     });
   });
+
+  test("It should return 422 without email for POST method", done => {
+    return request
+      .post("/users")
+      .send({ user: { password: "1234" } })
+      .then(response => {
+        expect(response.statusCode).toBe(422);
+        done();
+      });
+  });
+
+  test("It should return 422 without password for POST method", done => {
+    return request
+      .post("/users")
+      .send({ user: { email: "jdoe@gmail.com" } })
+      .then(response => {
+        expect(response.statusCode).toBe(422);
+        done();
+      });
+  });
 });
